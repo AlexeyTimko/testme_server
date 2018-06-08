2851b92a0b698adc4165ae38ce6b96b589ae28cc
alexeytimko <alexeytimko@gmail.com>
2018-06-08 15:28:36 +0300

    user auth

--- src/routers/user/auth.js
+++ src/routers/user/auth.js
@@ -1,5 +1,7 @@
 import db from '../../models/db';
 import sha256 from 'sha256';
+import jwt from "jsonwebtoken";
+import config from "../../config";
 
 const fields = {
     email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
@@ -19,7 +21,7 @@ const validate = data => {
     return valid;
 };
 
-export default (data, success, error) => {
+const authEmail = (data, success, error) => {
     if(!validate(data)){
         error('Invalid data');
     }else{
@@ -38,5 +40,33 @@ export default (data, success, error) => {
             }
             success(user);
         });
+    }
+};
+
+const authToken = (token, success, error) => {
+    jwt.verify(token, config.secret, (err, user) => {
+        if (err) {
+            return error('Invalid token');
+        }
+        const sql = 'SELECT * FROM "user" WHERE email = $1 AND active = $2';
+        const params = [
+            user.email,
+            true
+        ];
+        db.query(sql, params, (err, res) => {
+            if (err) {
+                return error('Log in failed');
+            }
+            const user = res.rows[0];
+            success(user);
+        });
+    });
+};
+
+export default (data, success, error) => {
+    if(data.token){
+        authToken(data.token, success, error);
+    }else{
+        authEmail(data, success, error);
     }
 }
