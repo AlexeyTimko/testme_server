2f82450b1e023f684b20573b9be56844073ed590
alexeytimko <alexeytimko@gmail.com>
2018-06-13 14:20:13 +0300

    file upload

--- src/routers/file-upload.js
+++ src/routers/file-upload.js
@@ -4,14 +4,13 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     let imageFile = req.files.file;
-    res.json({file: `${__dirname}/../../public/${req.body.filename}.jpg`});
-    // imageFile.mv(`${__dirname}/../../public/${req.body.filename}.jpg`, function(err) {
-    //     if (err) {
-    //         return res.status(500).send(err);
-    //     }
-    //
-    //     res.json({file: `public/${req.body.filename}.jpg`});
-    // });
+    imageFile.mv(`${__dirname}/../../../public/${req.body.fileame}.jpg`, function(err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        res.json({file: `public/${req.body.name}.jpg`});
+    });
 });
 
 export default router;
