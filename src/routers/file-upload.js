4abab0cf5fd91669c764d3f92d0af6345fc7fe7a
alexeytimko <alexeytimko@gmail.com>
2018-06-13 15:09:16 +0300

    file upload

--- src/routers/file-upload.js
+++ src/routers/file-upload.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     let imageFile = req.files.file;
-    const fn = `/var/www/testme/build/img/${new Date()}${req.body.name}`;
+    const fn = `/var/www/testme/build/img/${(new Date()).getTime()}${req.body.name}`;
     imageFile.mv(fn, function(err) {
         if (err) {
             return res.status(500).send(err);
