664878353efb84d31dac92f63d39407f0c6e1ebb
alexeytimko <alexeytimko@gmail.com>
2018-06-15 12:57:34 +0300

    test add

--- src/routers/test/add.js
+++ src/routers/test/add.js
@@ -22,31 +22,85 @@ const validate = data => {
     return valid;
 };
 
+const shouldAbort = (err) => {
+    if (err) {
+        console.error('Error in transaction', err.stack)
+        db.query('ROLLBACK', (err) => {
+            if (err) {
+                console.error('Error rolling back client', err.stack)
+            }
+        })
+    }
+    return !!err
+};
+
 export default (data, success, error) => {
     if(!validate(data)){
         error('Invalid data');
     }else{
-        const sql = 'INSERT INTO test (name, description, isprivate, islimited, timelimit, image, "user") VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING id';
-        const params = [
-            data.name,
-            data.description,
-            data.isPrivate,
-            data.isLimited,
-            data.timeLimit,
-            data.image,
-            data.user,
-        ];
-        db.query(sql, params, (err, res) => {
-            if (err) {
+        db.query('BEGIN', err => {
+            if (shouldAbort(err)) {
                 return error('Saving failed');
             }
-            const id = res.rows[0].id;
-            const sql = 'SELECT * FROM test WHERE id = $1';
-            db.query(sql, [ id ], (err, res) => {
-                if (err) {
+            const sql = 'INSERT INTO test (name, description, isprivate, islimited, timelimit, image, "user") VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING id';
+            const params = [
+                data.name,
+                data.description,
+                data.isPrivate,
+                data.isLimited,
+                data.timeLimit,
+                data.image,
+                data.user,
+            ];
+            db.query(sql, params, (err, res) => {
+                if (shouldAbort(err)) {
                     return error('Saving failed');
                 }
-                success(id);
+                const id = res.rows[0].id;
+                const sql = 'SELECT * FROM test WHERE id = $1';
+                db.query(sql, [ id ], (err, res) => {
+                    if (shouldAbort(err)) {
+                        return error('Saving failed');
+                    }
+                    for(let i = 0; i < data.questions.length; i++){
+                        let q = data.questions[i];
+                        const sql = 'INSERT INTO question (test, "text", image, weight) VALUES ($1,$2,$3,$4) RETURNING id';
+                        const params = [
+                            id,
+                            q.text,
+                            q.image,
+                            q.weight,
+                        ];
+                        db.query(sql, params, (err, res) => {
+                            if (shouldAbort(err)) {
+                                return error('Saving failed');
+                            }
+                            const qId = res.rows[0].id;
+                            for(let j = 0; j < q.answers.length; j++){
+                                let a = q.answers[j];
+                                const sql = 'INSERT INTO answer (question, "text", correct) VALUES ($1,$2,$3)';
+                                const params = [
+                                    qId,
+                                    a.text,
+                                    q.correct,
+                                ];
+                                db.query(sql, params, (err, res) => {
+                                    if (shouldAbort(err)) {
+                                        return error('Saving failed');
+                                    }
+                                    if(i === data.questions.length-1 && j === q.answers.length-1){
+                                        db.query('COMMIT', (err) => {
+                                            if (shouldAbort(err)) {
+                                                return error('Saving failed');
+                                            }
+                                            success(id);
+                                        })
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
             });
         });
     }
