febb5ea758204ca7b8d02c24e4d419ebf1c7a9f1
Spard <alexeytimko@gmail.com>
2018-06-16 09:50:49 +0300

    get current test

--- src/routers/test/load.js
+++ src/routers/test/load.js
@@ -1,11 +1,17 @@
 import db from '../../models/db';
 
 export default (id, success, error) => {
-    const sql = 'SELECT * FROM test WHERE id = $1';
+    const sql = '' +
+        'SELECT row_to_json(t) ' +
+        'FROM ( SELECT t.*, json_agg(json_build_object(q.*)) as questions ' +
+            'FROM test t ' +
+                'JOIN question q ON t.id = q.test ' +
+        'WHERE t.id = $1 GROUP BY t.id) t';
     db.query(sql, [id], (err, res) => {
         if (err) {
             return error('Network Error');
         }
-        success(res.rows[0]);
+        let test = res.rows[0];
+        success(test);
     });
 }
