d3b4a11a2b97cc846a960a6a20f34bdde9f33717
Spard <alexeytimko@gmail.com>
2018-06-16 11:00:00 +0300

    get current test

--- src/routers/test/load.js
+++ src/routers/test/load.js
@@ -1,12 +1,23 @@
 import db from '../../models/db';
 
 export default (id, success, error) => {
-    const sql = '' +
-        'SELECT row_to_json(t) ' +
-        'FROM ( SELECT t.*, json_agg(json_build_object(q.*)) as questions ' +
-            'FROM test t ' +
-                'JOIN question q ON t.id = q.test ' +
-        'WHERE t.id = $1 GROUP BY t.id) t';
+    const sql = 'select\n' +
+        '  t.*,\n' +
+        '  json_agg(\n' +
+        '      json_build_object(\n' +
+        '          \'id\', q.id, \'text\', q.text, \'image\', q.image, \'weight\', q.weight, \'answers\',\n' +
+        '          (\n' +
+        '            select json_agg(json_build_object(\'id\', a.id, \'text\', a.text, \'correct\', a.correct))\n' +
+        '            from answer a\n' +
+        '            where a.question = q.id\n' +
+        '            group by q.id\n' +
+        '          )\n' +
+        '      )\n' +
+        '  ) as questions\n' +
+        'from test t\n' +
+        '  join question q on q.test = t.id\n' +
+        'where t.id = $1\n' +
+        'group by t.id';
     db.query(sql, [id], (err, res) => {
         if (err) {
             return error('Network Error');
