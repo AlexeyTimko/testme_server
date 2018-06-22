d7120aa68bdc272d81f8717586057266b40e89e2
alexeytimko <alexeytimko@gmail.com>
2018-06-22 13:53:23 +0300

    pagination

--- src/routers/test/load.js
+++ src/routers/test/load.js
@@ -1,6 +1,7 @@
 import db from '../../models/db';
 
 export default (id, success, error) => {
+    let params = [id];
     let sql = 'select\n' +
         '  t.*,\n' +
         '  json_agg(\n' +
@@ -18,10 +19,12 @@ export default (id, success, error) => {
         '  join question q on q.test = t.id\n';
     if(id === 'rand'){
         sql += 'group by t.id limit 1 order by random() < 0.01 limit 1';
+        params = [];
     }else{
         sql += 'where t.id = $1 group by t.id';
     }
-    db.query(sql, [id], (err, res) => {
+    console.log(sql);
+    db.query(sql, params, (err, res) => {
         if (err) {
             return error('Network Error');
         }
